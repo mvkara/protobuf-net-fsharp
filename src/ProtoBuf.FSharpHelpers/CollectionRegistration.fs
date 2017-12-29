4c17019a449d8a018acbeb77eaf515f22b4770c7
mvkra <markvkara@gmail.com>
2017-12-29 12:05:51 +1100

    draft poc for never-null arrays and F# lists for registered records and DU's + custom registration (Note: Needs PR from https://github.com/mgravell/protobuf-net/pull/336 to work)

---
+++ src/ProtoBuf.FSharpHelpers/CollectionRegistration.fs
@@ -0,0 +1,69 @@
+module ProtoBuf.FSharpHelpers.CollectionRegistration
+
+open ProtoBuf.Meta
+open ProtoBuf
+open System
+
+[<ProtoContract>]
+type ListSurrogate<'t>() =
+    [<ProtoMember(1)>]
+    member val public List = Array.empty<'t> with get, set
+
+    static member public To (l: 't list) = 
+        match box l with
+        | null -> ListSurrogate(List = Array.empty<'t>)
+        | _ when List.isEmpty l -> ListSurrogate(List = Array.empty<'t>)
+        | _ -> ListSurrogate(List = (l |> Seq.toArray))
+
+    static member public From (l: 't ListSurrogate) = 
+        match box l.List with
+        | null -> []
+        | _ -> ((l.List) |> List.ofSeq)
+     
+    static member public op_Implicit (l: 't list) : ListSurrogate<'t> = ListSurrogate<'t>.To(l) 
+    static member public op_Implicit (l: 't ListSurrogate) : 't list = ListSurrogate<'t>.From(l) 
+
+[<ProtoContract>]
+type ArraySurrogate<'t>() =
+    [<ProtoMember(1)>]
+    member val public List = Array.empty<'t> with get, set
+
+    static member public To (l: 't array) = 
+        match l with
+        | null -> ArraySurrogate(List = Array.empty<'t>)
+        | _ when Array.isEmpty l -> ArraySurrogate(List = Array.empty<'t>)
+        | _ -> ArraySurrogate(List = (l |> Seq.toArray))
+
+    static member public From (l: 't ArraySurrogate) = 
+        match box l.List with
+        | null -> [||]
+        | _ -> l.List
+     
+    static member public op_Implicit (l: 't array) : ArraySurrogate<'t> = ArraySurrogate<'t>.To(l) 
+    static member public op_Implicit (l: 't ArraySurrogate) : 't array = ArraySurrogate<'t>.From(l) 
+
+let public registerCollectionWithReflectedTypeIntoModel (model: RuntimeTypeModel) (listType: Type) = 
+    let registerFunc (surrogateType: Type) = 
+        let itemType = listType.GetGenericArguments().[0]
+        let collectionSurrogateType = surrogateType.MakeGenericType(itemType)
+        let t = model.Add(listType, false)
+        t.IgnoreListHandling <- true // Bug: MetaType.ResolveListTypes does not respect IgnoreListHandling = true flag.
+        t.SetSurrogate(collectionSurrogateType)
+        model
+    
+    let surrogateTypeOpt = 
+        lazy
+            if listType.IsGenericType
+            then
+                match (listType.GetGenericTypeDefinition()) with
+                | x when x = (typedefof<list<_>>) -> typedefof<ListSurrogate<_>> |> Some
+                | x when x = (typedefof<_ array>) -> typedefof<ArraySurrogate<_>> |> Some
+                | _ -> None
+            else None            
+    
+    if (model.IsDefined(listType) || Option.isNone surrogateTypeOpt.Value) 
+    then model 
+    else registerFunc (surrogateTypeOpt.Value |> Option.get)      
+
+let public registerListTypeIntoModel<'t> (model: RuntimeTypeModel) = 
+   registerCollectionWithReflectedTypeIntoModel model typeof<'t list>
--- test/ProtoBuf.FSharpHelpers.Unit/TestRecordRoundtrip.fs
+++ test/ProtoBuf.FSharpHelpers.Unit/TestRecordRoundtrip.fs
@@ -10,45 +10,43 @@ open Expecto.Expect
 open System
 open System.Collections.Generic
 open System.Linq
+open System.Runtime.CompilerServices
 
 [<TestName("Standard record with a simple C# list")>]
 type TestRecordOne = {
     One: string
     Two: int
-    Three: string[]
+    Three: string list
 }
 
 module TestRecordRoundtrip = 
 
     type DataGenerator =
-        static member Generate() : Arbitrary<string[]> = 
-            Gen.oneof ([ "One"; "Two"; "" ] |> List.map Gen.constant) 
-            |> Gen.listOf
-            |> Gen.map List.toArray
-            |> Arb.fromGen
             
-        static member GenerateNonNullString() : Arbitrary<string> = Arb.Default.StringWithoutNullChars().Generator |> Gen.map (fun x -> x.Get) |> Arb.fromGen
-
+        static member GenerateNonNullString() : Arbitrary<string> = 
+            [ 1 .. 20 ]
+            |> Gen.elements
+            |> Gen.map (string)
+            |> Arb.fromGen
+                
     let propertyToTest<'t when 't : equality> cleanupFunc (typeToTest: 't)  = 
         let model = 
             RuntimeTypeModel.Create() 
-           // |> FSharpCollectionRegistration.registerListIntoModel<string>
-            |> ProtobufNetSerialiser.registerRecordIntoModel<'t>
+            //|> CollectionRegistration.registerListTypeIntoModel<string>
+            |> ProtobufNetFSharp.registerRecordIntoModel true typeof<'t>
 
         model.CompileInPlace()
-        let cloned = model.DeepClone(typeToTest)
-        equal (unbox cloned |> cleanupFunc) (cleanupFunc typeToTest) "Protobuf deep clone"
         use ms = new MemoryStream()
-        ProtobufNetSerialiser.serialise model ms typeToTest
+        ProtobufNetFSharp.serialise model ms typeToTest
         ms.Seek(0L, SeekOrigin.Begin) |> ignore
-        let rtData = ProtobufNetSerialiser.deserialise<'t> model ms
+        let rtData = ProtobufNetFSharp.deserialise<'t> model ms
         equal (cleanupFunc rtData) (cleanupFunc typeToTest) "Type not equal"
     
     let config = { Config.QuickThrowOnFailure with Arbitrary = [ typeof<DataGenerator> ] }
 
     let emptySingletonListForEqualityComparison = new List<string>() :> IList<string>
 
-    let cleanupNullForLists (t: TestRecordOne) = { t with Three = match t.Three with | null -> [||] | _ -> t.Three }
+    let cleanupNullForLists (t: TestRecordOne) = t//{ t with Three = match t.Three with | null -> [||] | _ -> t.Three }
 
     let buildTest<'t when 't : equality> cleanupNullForLists = 
         let testNameAttribute = typeof<'t>.GetCustomAttributes(typeof<TestNameAttribute>, true) |> Seq.head :?> TestNameAttribute
