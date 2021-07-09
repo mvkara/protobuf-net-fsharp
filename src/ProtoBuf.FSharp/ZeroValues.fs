0e2ec5f3f059df33a6cc124f5e3ede123d16c89a
Alexander Mishunin <alexander.mishunin@gmail.com>
2021-07-09 12:36:36 +0300

    Apply zero values in Option surrogate

--- src/ProtoBuf.FSharp/ZeroValues.fs
+++ src/ProtoBuf.FSharp/ZeroValues.fs
@@ -4,13 +4,13 @@ open System
 open System.Collections.Concurrent
 open System.Reflection
 
-module internal ZeroValues =
+module ZeroValues =
 
     type internal FieldWithZeroValueSetMethod = { FieldInfo: FieldInfo; ZeroValueMethod: MethodHelpers.MethodType }
 
     let private zeroValueFieldSetters = ConcurrentDictionary<Type, MethodHelpers.MethodType>()
 
-    let getZeroValueMethodInfoOpt (fieldType: Type) =
+    let internal getZeroValueMethodInfoOpt (fieldType: Type) =
         /// Creates the zero value for supported types that we know of.
         let createZeroValueMethodInfoSetter() =
             if fieldType = typeof<string> then
@@ -34,9 +34,30 @@ module internal ZeroValues =
                 Some zeroValue
             | None -> None
 
-    let calculateApplicableFields (fields: FieldInfo[]) = [|
+    let internal calculateApplicableFields (fields: FieldInfo[]) = [|
         for fi in fields do
             match getZeroValueMethodInfoOpt fi.FieldType with
             | None -> ()
             | Some getZeroValueMethod ->  { FieldInfo = fi; ZeroValueMethod = getZeroValueMethod }
     |]
+
+    let isApplicableTo (t : Type) =
+        getZeroValueMethodInfoOpt t |> Option.isSome
+
+    let getZeroValue<'t> () : 't =
+        match getZeroValueMethodInfoOpt typeof<'t> with
+        | Some (MethodHelpers.MethodType.NewArray elType) ->
+            System.Array.CreateInstance(elType, 0) |> unbox
+        | Some (MethodHelpers.MethodType.MethodInfo mi) ->
+            mi.Invoke(null, null) |> unbox
+        | Some (MethodHelpers.MethodType.FieldInfo fi) ->
+            fi.GetValue(null) |> unbox
+        | Some (MethodHelpers.MethodType.PropertyInfo pi) ->
+            pi.GetValue(null) |> unbox
+        | None ->
+            sprintf "No zero value for %A" typeof<'t> |> NotImplementedException |> raise
+
+    let internal fixZeroValue<'t> (x : 't) =
+        match box x with
+        | null -> getZeroValue<'t> ()
+        | _ -> x
